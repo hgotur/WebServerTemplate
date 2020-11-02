64bcca6cc24f5dfe068c391d7cd7ffc19059fb4a
Harsha Gotur <hgotur@gmail.com>
2020-11-02 09:43:22 -0500

    updated files

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -3,10 +3,19 @@ package service;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
+	static Map<String, List<String>> categoryToMembers = Map.of(
+			"fruits", List.of("apples", "bananas", "pears", "blueberries"),
+			"veggies", List.of("broccoli", "spinach", "eggplant")
+	);
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -14,4 +23,16 @@ public class MainController {
 		return "hello";
 	}
 
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/info/{category}")
+	public String category(@PathVariable("category") String category, Model model) {
+		List<String> members = categoryToMembers.getOrDefault(category, new ArrayList<>());
+		model.addAttribute("category", category);
+		model.addAttribute("members", members);
+		return "info";
+	}
 }
