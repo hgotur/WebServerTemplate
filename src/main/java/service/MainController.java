c4bc5f4a01d745bea3424a350eef7f8dcf1c77d8
Harsha Gotur <hgotur@gmail.com>
2020-11-04 09:55:53 -0500

    Revert "Merge pull request #1 from hgotur/fruits"
    This reverts commit ebf119da71a148fa048fd6bc7d60415e71ff8e34, reversing
    changes made to 04750824b303f588b9de49f25a27a9b14ca8f9f0.

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -3,40 +3,15 @@ package service;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 public class MainController {
-	static Map<String, List<String>> categoryToMembers = Map.of(
-			"fruits", List.of("apples", "bananas", "pears", "blueberries"),
-			"veggies", List.of("broccoli", "spinach", "eggplant")
-	);
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "hello";
-	}
-
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
-
-	@GetMapping("/info/{category}")
-	public String category(@PathVariable("category") String category, Model model) {
-		String name = "Harsha";
-		List<String> members = categoryToMembers.getOrDefault(category, new ArrayList<>());
-		model.addAttribute("category", category);
-		model.addAttribute("name", name);
-		model.addAttribute("members", members);
-
-		return "info";
 	}
 
 }
